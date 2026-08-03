bbfc079aae711921e87a16cedf39e69db7bafe1d
HautevilleHouse <239203806+HautevilleHouse@users.noreply.github.com>
2026-08-03 19:13:55 -0400

    Canon

---
+++ abstract_axiomatic_homotopy_theory_algebraic_lemma_canonical_lane_mathlib/FinalTheorem.lean
@@ -0,0 +1,9 @@
+import abstract_axiomatic_homotopy_theory_algebraic_lemma_canonical_lane_mathlib.GateLemmas
+namespace HautevilleHouse
+namespace abstract_axiomatic_homotopy_theory_algebraic_lemma_canonical_lane_mathlib
+structure ConstrainedTheoremClosure where
+  admissible :: intro :: constraint :: consequence :: proof ::
+theorem canonical : ConstrainedTheoremClosure :=
+  ConstrainedTheoremClosure.intro
+end abstract_axiomatic_homotopy_theory_algebraic_lemma_canonical_lane_mathlib
+end HautevilleHouse
